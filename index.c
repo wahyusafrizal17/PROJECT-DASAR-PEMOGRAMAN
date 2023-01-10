6203e5497475e013142a475b0154aa6fa9223f8b
xyzjgmday <daysubhiz@gmail.com>
2023-01-10 20:57:42 +0700

    menambah proses menu

--- index.c
+++ index.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-//mmk
+#include "menu.c"
+
 char username[10], pass[10];
 
-void mainmenu(menu){
-	if(menu == "menu1"){
-		printf("Semua Menu");
-	}else if(menu == "menu2"){
-		printf("Menu2");
-	}else if(menu == "menu3"){
-		printf("Menu3");
-	}
-}
+void menu();
 
 void main(){
 	
@@ -18,20 +11,20 @@ void main(){
 	scanf("%s", &username);
 	printf("Masukan Password anda\t: ");
 	scanf("%s", &pass);
-	if((strcmp(username,"alma")==0 && strcmp(pass,"allers")==0)){
-		printf("\nSelamat Datang ALLERS\n");
-		printf("Anda bisa menggunakan semua fitur dibawah ini\t:\n");
-		mainmenu("menu1");
+	if((strcmp(username,"dayat")==0 && strcmp(pass,"kataers")==0)){
+			printf("\nSelamat Datang Kataers\n");
+			printf("Anda bisa menggunakan semua fitur dibawah ini\t:\n");
+			mainmenu("menuKata");
 	}
-	else if((strcmp(username,"dayat")==0 && strcmp(pass,"angkaers")==0)){
+	else if((strcmp(username,"wahyu")==0 && strcmp(pass,"angkaers")==0)){
 			printf("\nSelamat Datang ANGKAERS\n");
 			printf("Anda bisa menggunakan semua fitur dibawah ini\t:\n");
-			mainmenu("menu2");
+			mainmenu("menuKata");
 	}
-	else if((strcmp(username,"wayat")==0 && strcmp(pass,"kataers")==0)){
-			printf("\nSelamat Datang KATAERS\n");
-			printf("Anda bisa menggunakan semua fitur dibawah ini\t:\n");
-			mainmenu("menu3");
+	else if((strcmp(username,"alma")==0 && strcmp(pass,"allers")==0)){
+		printf("\nSelamat Datang ALLERS\n");
+		printf("Anda bisa menggunakan semua fitur dibawah ini\t:\n");
+		mainmenu("menuSemua");
 	}else{
 		printf("\nUsername / Password yang anda masukan salah\n");
 		system("pause");
---
+++ menu.c
@@ -0,0 +1,9 @@
+void mainmenu(menu){
+	if(menu == "menuKata"){
+		printf("Menu Kata");
+	}else if(menu == "menuAngka"){
+		printf("Menu Angka");
+	}else if(menu == "menuSemua"){
+		printf("VIP");
+	}
+}
